7d62e08a1648e76745fc5c1754825abbc7e5a4c1
noliw <noliw2017@gmail.com>
2023-10-01 02:57:27 -0700

    final push

--- app/src/main/java/com/example/tictactoe/AiActivity.kt
+++ app/src/main/java/com/example/tictactoe/AiActivity.kt
@@ -2,17 +2,18 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import com.example.tictactoe.databinding.ActivityMainBinding
-import com.example.tictactoe.databinding.ActivityTwoPlayerBinding
+import com.example.tictactoe.databinding.ActivityAiBinding
 
-class TwoPlayerActivity : AppCompatActivity() {
+class AiActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityTwoPlayerBinding
+    private lateinit var binding: ActivityAiBinding
     private var turn = "1"
+    private var isDialogShowing = false
+
     private var moveCounter = 0   // Counter to track number of moves
     private var isGameActive = true
     private val allImageViews by lazy {
@@ -27,7 +28,7 @@ class TwoPlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityTwoPlayerBinding.inflate(layoutInflater)
+        binding = ActivityAiBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.playAgainBtn.setOnClickListener { reset() }
@@ -41,27 +42,41 @@ class TwoPlayerActivity : AppCompatActivity() {
         }
     }
 
+    fun ai() {
+        if (!isGameActive) return  // Don't play if game is over
+
+        // Introduce a delay using Handler
+        Handler(Looper.getMainLooper()).postDelayed({
+            val emptyCells = allImageViews.filter { it.tag == null }
+            if (emptyCells.isNotEmpty()) {
+                val randomCell = emptyCells.random()
+                randomCell.setImageResource(R.drawable.o)
+                randomCell.tag = "2"
+                moveCounter++
+                win()// Check if AI won after making its move
+            }  // Delay of 1 second (1000 milliseconds
+        }, 500)
+    }
+
 
     private fun buttonClick(btn: ImageView) {
         // Check if ImageView is empty by its tag
-        if (isGameActive && btn.tag == null ) {
-            turn = if (turn == "1") {
-                btn.setImageResource(R.drawable.x)
-                btn.tag = "1"
-                moveCounter++  // Increment the move counter
-                "2"
-            } else {
-                btn.setImageResource(R.drawable.o)
-                btn.tag = "2"
-                moveCounter++  // Increment the move counter
-                "1"
+        if (isGameActive && btn.tag == null) {
+            btn.setImageResource(R.drawable.x)
+            btn.tag = "1"
+            moveCounter++
+            win()
+
+            // Only let the AI make a move if the game is still active AFTER checking for player 1 win
+            if (isGameActive) {
+                ai()  // AI makes its move after Player 1
             }
         }
     }
 
+
     fun win() {
-        // Player 1 win conditions
-        val p1Win = setOf(
+        val win = setOf(
             listOf(binding.imageView, binding.imageView2, binding.imageView3),
             listOf(binding.imageView4, binding.imageView5, binding.imageView6),
             listOf(binding.imageView7, binding.imageView8, binding.imageView9),
@@ -69,69 +84,53 @@ class TwoPlayerActivity : AppCompatActivity() {
             listOf(binding.imageView2, binding.imageView5, binding.imageView8),
             listOf(binding.imageView3, binding.imageView6, binding.imageView9),
             listOf(binding.imageView, binding.imageView5, binding.imageView9),
-            listOf(binding.imageView3, binding.imageView5, binding.imageView7),
-            // ... add the other win conditions for Player 1
+            listOf(binding.imageView3, binding.imageView5, binding.imageView7)
         )
 
-        // Player 2 win conditions
-        val p2Win = setOf(
-            listOf(binding.imageView, binding.imageView2, binding.imageView3),
-            listOf(binding.imageView4, binding.imageView5, binding.imageView6),
-            listOf(binding.imageView7, binding.imageView8, binding.imageView9),
-            listOf(binding.imageView, binding.imageView4, binding.imageView7),
-            listOf(binding.imageView2, binding.imageView5, binding.imageView8),
-            listOf(binding.imageView3, binding.imageView6, binding.imageView9),
-            listOf(binding.imageView, binding.imageView5, binding.imageView9),
-            listOf(binding.imageView3, binding.imageView5, binding.imageView7),
-
-            // ... add the other win conditions for Player 2
-        )
-
-        for (condition in p1Win) {
+        for (condition in win) {
             if (condition.all { it.tag == "1" }) {
+                isGameActive = false
                 showDialog("Player 1 has won", "CONGRATULATIONS PLAYER 1")
-                isGameActive = false
                 return
             }
-        }
-
-        for (condition in p2Win) {
             if (condition.all { it.tag == "2" }) {
-                showDialog("Player 2 has won", "CONGRATULATIONS PLAYER 2")
                 isGameActive = false
+                showDialog("computer has won", "YOU LOST ")
                 return
             }
         }
 
-        // Check for a tie if all cells are occupied
-        if (moveCounter == 9) {
+        if (moveCounter == 9 && isGameActive) {
             showDialog("It's a tie!", "Aweeeee")
             isGameActive = false
         }
     }
 
     private fun showDialog(message: String, title: String) {
+        if (isDialogShowing) return
+
         val dialogBuilder = AlertDialog.Builder(this)
         dialogBuilder.setMessage(message)
             .setTitle(title)
-            .setCancelable(true)
+            .setCancelable(false)
             .setPositiveButton("Go back") { dialog, _ ->
-                dialog.cancel()
+                isDialogShowing = false
+                dialog.dismiss()
             }
-            .setNegativeButton("Reset") { _, _ ->
+            .setNegativeButton("Reset") { dialog, _ ->
                 reset()
+                isDialogShowing = false
+                dialog.dismiss()
             }
-            .show()  // Show the dialog
+            .show()
+
+        isDialogShowing = true
     }
 
-    private fun reset() {
-        isGameActive = true
-        val allImageViews = listOf(
-            binding.imageView, binding.imageView2, binding.imageView3,
-            binding.imageView4, binding.imageView5, binding.imageView6,
-            binding.imageView7, binding.imageView8, binding.imageView9
-        )
 
+
+
+    private fun reset() {
         for (imageView in allImageViews) {
             imageView.tag = null
             imageView.setImageResource(0)  // Clear the image
--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.tictactoe
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var start = findViewById<Button>(R.id.startGameButton)
+        var ai = findViewById<Button>(R.id.Ai)
 
         start.setOnClickListener {
             var t = Intent(this, TwoPlayerActivity::class.java)
+            startActivity(t)
+        }
+            ai.setOnClickListener {
+            var t = Intent(this, AiActivity::class.java)
             startActivity(t)
         }
 
